a3470fd3325396efa3a0e376f31af7c3198307c4
salmasgh <selma.sghaier@insat.ucar.tn>
2025-03-27 00:23:01 +0100

    image uploading logic

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UploadModule } from './upload/upload.module';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+      serveRoot: '/public',
+    }),
+    UploadModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
