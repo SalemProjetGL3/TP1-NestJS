5a5e414f864c9ddec904dcdcd4000023905b7f22
salmasgh <selma.sghaier@insat.ucar.tn>
2025-04-10 18:21:18 +0100

    Fixing file sie and exposing public directory

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// import { MulterExceptionFilter } from './filters/multer-exception.filter';
+import * as express from 'express';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Expose the 'public/uploads' folder as static
+  app.use('/uploads', express.static(join(__dirname, '..', 'public', 'uploads')));
+
+  // Register the global filter
+  // app.useGlobalFilters(new MulterExceptionFilter());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -10,38 +10,39 @@ export class UploadController {
     @UseInterceptors(
     FileInterceptor('cv', {
         storage: diskStorage({
-        destination: './public/cvs',
-        filename: (req, file, callback) => {
-            const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-            const ext = extname(file.originalname);
-            const filename = `${uniqueSuffix}${ext}`;
-            callback(null, filename);
-        },
-        }),
+            destination: join(process.cwd(), 'public', 'uploads'),
+            filename: (req, file, callback) => {
+              const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+              const ext = extname(file.originalname);
+              const filename = `${uniqueSuffix}${ext}`;
+              callback(null, filename);
+            },
+        }),          
         fileFilter: (req, file, callback) => {
-        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/i)) {
+            console.log('File type not allowed!');
             return callback(new BadRequestException('Only JPG/JPEG/PNG files are allowed!'), false);
         }
         callback(null, true);
         },
         limits: {
-        fileSize: 1024 * 1024 * 5, // 5MB
+            fileSize: 1024 * 1024 , // 1MB
         },
     }),
     )
     async uploadCv(@UploadedFile() file: Express.Multer.File) {
-    if (!file) {
-        throw new BadRequestException('File is required');
-    }
-    return {
-        originalname: file.originalname,
-        filename: file.filename,
-    };
+        if (!file) {
+            throw new BadRequestException('File is required');
+        }
+        return {
+            originalname: file.originalname,
+            filename: file.filename,
+        };
     }
 
     @Get('cv/:filename')
     async getCv(@Param('filename') filename: string, @Res() res: Response) {
-    const filePath = join(process.cwd(), 'public/cvs', filename);
-    res.sendFile(filePath);
+        const filePath = join(process.cwd(), 'public/cvs', filename);
+        res.sendFile(filePath);
     }
 }
